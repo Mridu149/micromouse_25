2eba10d547ddb121c8029aee53022eb05b91634d
neo-0007 <hrishikeshgohain123@gmail.com>
2025-08-25 20:30:24 +0530

    feat: add driver code for vl53l0x sensors

---
+++ firmware/mouse_v1/sensors.h
@@ -0,0 +1,66 @@
+#ifndef SENSORS_H
+#define SENSORS_H
+
+#include "config.h"
+
+#include <Wire.h>
+#include <VL53L0X.h>
+
+class IRSensor
+{
+public:
+  void init()
+  {
+    Wire.begin();
+
+    pinMode(IR_XSHUT_1, OUTPUT);
+    pinMode(IR_XSHUT_2, OUTPUT);
+    pinMode(IR_XSHUT_1, OUTPUT);
+
+    digitalWrite(IR_XSHUT_1, LOW);
+    digitalWrite(IR_XSHUT_2, LOW);
+    digitalWrite(IR_XSHUT_3, LOW);
+    delay(10);
+
+    digitalWrite(IR_XSHUT_1, HIGH);
+    delay(10);
+    sensor1.init();
+    sensor1.setAddress(0x30);
+
+    digitalWrite(IR_XSHUT_2, HIGH);
+    delay(10);
+    sensor2.init();
+    sensor2.setAddress(0x31);
+
+    digitalWrite(IR_XSHUT_3, HIGH);
+    delay(10);
+    sensor3.init();
+    sensor3.setAddress(0x32);
+
+    sensor1.startContinuous();
+    sensor2.startContinuous();
+    sensor3.startContinuous();
+  }
+
+  uint16_t getFirstSensorReading()
+  {
+    return sensor1.readRangeContinuousMillimeters();
+  }
+
+  uint16_t getSecondSensorReading()
+  {
+    return sensor2.readRangeContinuousMillimeters();
+  }
+
+  uint16_t getThirdSensorReading()
+  {
+    return sensor3.readRangeContinuousMillimeters();
+  }
+
+private:
+  VL53L0X sensor1;
+  VL53L0X sensor2;
+  VL53L0X sensor3;
+};
+
+#endif
