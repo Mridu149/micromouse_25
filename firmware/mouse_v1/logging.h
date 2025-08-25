131048d503e274e0b8f695048b6fe68319c9cf97
neo-0007 <hrishikeshgohain123@gmail.com>
2025-08-26 02:12:58 +0530

    feat: add logging using wifi & bt

--- firmware/mouse_v1/logging.h
+++ firmware/mouse_v1/logging.h
@@ -1,3 +1,19 @@
+#define BLUETOOTH_DEBUGGING
+//#define WIFI_DEBUGGING
+
+#ifdef BLUETOOTH_DEBUGGING
+#include "BluetoothSerial.h"
+BluetoothSerial SerialBT;
+
+void initBluetooth(){
+  Serial.begin(115200);
+  SerialBT.begin("ESP32_BT");  // Bluetooth name
+  Serial.println("Bluetooth ready. Connect to ESP32_BT");
+}
+
+#endif
+
+#ifdef WIFI_DEBUGGING
 #include <Arduino.h>
 #include <WiFi.h>
 #include <AsyncTCP.h>
@@ -5,13 +21,12 @@
 #include <WebSerial.h>
 
 AsyncWebServer server(80);
-
-const char* ssid = "Phone 1_4549";          // Your WiFi SSID
-const char* password = "ggboisss";  // Your WiFi Password
+const char* ssid = "Phone 1_4549";      // WiFi SSID
+const char* password = "ggboisss";      // WiFi Password
 
 void initWebServer() {
   Serial.begin(115200);
- 
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   if (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -23,4 +38,15 @@ void initWebServer() {
 
   WebSerial.begin(&server);
   server.begin();
+}
+#endif
+
+void initLogging() {
+#ifdef BLUETOOTH_DEBUGGING
+  initBluetooth();
+#endif
+
+#ifdef WIFI_DEBUGGING
+  initWebServer();
+#endif
 }
